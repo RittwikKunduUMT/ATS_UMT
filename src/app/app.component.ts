abcd541d75f99774b44ef294fd82668b78ace2c9
RITTWIK KUNDU <rrittwikkundu@gmail.com>
2024-05-03 17:05:39 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  constructor(private modalService: NgbModal) {}
   title = 'ATS_Project';
+  isModalOpen = false;
+
+  toggleModal() {
+    this.isModalOpen = !this.isModalOpen;
+  }
+  // openModal(content: any) {
+  //   this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' });
+  // }
 }
